caad31f411f09adbd76b97770028650394d5d494
Cody Persinger <cody.persinger@intouchsol.com>
2020-03-11 13:20:48 -0500

    Bump package version

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -3,14 +3,7 @@ const { createWebpackConfig } = require('@intouchgroup/es2020-boilerplate');
 module.exports = (env, argv) => createWebpackConfig({
     argv,
     entries: {
-        primary: './scripts/index.js',
-        styles: './styles/styles.scss',
-        'styles/test999': './styles/test/test.scss',
-        'nopoly/primary': {
-            file: './scripts/index.js',
-            plugins: [],
-            polyfill: false,
-        },
+        promise: './scripts/index.js',
     },
     nodeModulesToBabel: [],
 });
