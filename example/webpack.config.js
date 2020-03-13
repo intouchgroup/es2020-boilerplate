9d3d13738f28e1faf982b15835c1dd0de1209fa8
Cody Persinger <cody.persinger@intouchsol.com>
2020-03-13 12:44:28 -0500

    Remove skipStyles option, and configure StyleLint to fail silently with empty input

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -3,7 +3,14 @@ const { createWebpackConfig } = require('@intouchgroup/es2020-boilerplate');
 module.exports = (env, argv) => createWebpackConfig({
     argv,
     entries: {
-        promise: './scripts/index.js',
+        primary: './scripts/index.js',
+        styles: './styles/styles.scss',
+        'styles/test999': './styles/test/test.scss',
+        'nopoly/primary': {
+            file: './scripts/index.js',
+            plugins: [],
+            polyfill: false,
+        },
     },
     nodeModulesToBabel: [],
 });
